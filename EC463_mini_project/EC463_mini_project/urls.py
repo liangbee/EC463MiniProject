84ad35f92cfadb310962830ffc7c3299c9e45f6b
Biyao Liang <liangb@bu.edu>
2018-09-12 13:16:15 -0400

    update url.py
    see if this works

--- EC463_mini_project/EC463_mini_project/urls.py
+++ EC463_mini_project/EC463_mini_project/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from catalog import views
+from django.contrib import admin
+from django.conf.urls import include
 
+# Use include() to add paths from the catalog application
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
 ]
+
