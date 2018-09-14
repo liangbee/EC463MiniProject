f7f61905c11e0d27e439dc50fc7e60072d733092
Biyao Liang <liangb@bu.edu>
2018-09-13 22:07:33 -0400

    update new url file

--- EC463_mini_project/EC463_mini_project/urls.py
+++ EC463_mini_project/EC463_mini_project/urls.py
@@ -1,10 +1,25 @@
 from django.urls import path
 from django.contrib import admin
 from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView #Add URL maps to redirect the base URL to our application
 
-# Use include() to add paths from the catalog application
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
+urlpatterns += [
+	path('catalog/', include('catalog.urls')),
+]
+
+urlpatterns += [
+    path('', RedirectView.as_view(url='/catalog/')),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
