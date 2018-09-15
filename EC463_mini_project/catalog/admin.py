95f6c1922cbf4aecfa7f9db79e139a20454ff245
Mohammad Ali Al-Rajjal <mohd.ali.rijjal@gmail.com>
2018-09-15 14:06:00 -0400

    Update admin.py

--- EC463_mini_project/catalog/admin.py
+++ EC463_mini_project/catalog/admin.py
@@ -3,7 +3,6 @@ from catalog.models import HouseRoom,Profile
 
 admin.site.register(HouseRoom)
 
-
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
