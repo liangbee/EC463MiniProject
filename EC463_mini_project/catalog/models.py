4717dfb58e56bc4f050fc36e61de862ec34a0113
mohdalirijjal <mohd.ali.rijjal@gmail.com>
2018-09-15 12:47:51 -0400

    added inline model

--- EC463_mini_project/catalog/models.py
+++ EC463_mini_project/catalog/models.py
@@ -6,4 +6,14 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     number_of_rooms = models.IntegerField()
 
+class HouseRoom(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    temperature_reading = models.CharField(max_length=100)
+    humidity_reading = models.CharField(max_length=100)
+    
+    
+    def __str__(self):
+        return self.name
+
+
 
