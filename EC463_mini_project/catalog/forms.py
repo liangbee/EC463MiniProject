7046bff2d8094fee9e88aff496e42bce73935162
mohdalirijjal <mohd.ali.rijjal@gmail.com>
2018-09-15 13:45:04 -0400

    added signup form

--- EC463_mini_project/catalog/forms.py
+++ EC463_mini_project/catalog/forms.py
@@ -1,3 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+
+
+class SignUpForm(UserCreationForm):
+    number_of_rooms = forms.IntegerField(help_text='Enter the number of rooms in your household', min_value=1)
